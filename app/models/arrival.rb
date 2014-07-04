53e7cefdb5163ca7da93d2cafabbb56b4acd99eb
Russell Stern <skier2k5@gmail.com>
2014-07-04 19:45:48 -0400

    Added client scope to arrivals

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -5,6 +5,7 @@ class Arrival < ActiveRecord::Base
     self.where(:id => arrival_id).first ||
     self.where(:user_agent => user_agent)
         .where(:ip => ip)
+        .where(:client_id => Client.current.id)
         .where("created_at > (NOW() - INTERVAL 1 DAY)")
         .order("created_at desc")
         .first
