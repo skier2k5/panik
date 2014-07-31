4cc9670575f880fcb09247ca35b49fea17dbd7d3
Lawrence Xia <xialairei@gmail.com>
2014-07-30 21:35:22 -0400

    remove redundant controller and index files

--- app/controllers/index_controller.rb
+++
@@ -1,8 +0,0 @@
-class IndexController < TrackedController
-  def index
-    case Client.current.name
-    when 'celebheadlines'
-      @headlines = CelebHeadline.all.order(:posted_at)
-    end
-  end
-end
