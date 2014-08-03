7a68fbbeef441d47d820d418748ecd912a64539a
Lawrence Xia <xialairei@gmail.com>
2014-08-03 14:35:51 -0400

    replace tab indentation with spaces: 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   #Takes title of client, and adds the title of the current page
   def full_title(page_title)
-	base_title = Client.current.title
-	if page_title.empty?
-	  base_title
-	else
-	  "#{base_title} | #{page_title}"
-	end
+    base_title = Client.current.title
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
   end
 
 end
