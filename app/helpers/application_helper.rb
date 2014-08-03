93c94478e20db5992ff253a48d03b397f5d19bf2
Lawrence Xia <xialairei@gmail.com>
2014-08-03 14:23:00 -0400

    fix tabs to 2 spaces, correct spelling error in footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
 
-	#Takes title of client, and adds the title of the current page
-	def full_title(page_title)
-		base_title = Client.current.title
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
+  #Takes title of client, and adds the title of the current page
+  def full_title(page_title)
+	base_title = Client.current.title
+	if page_title.empty?
+	  base_title
+	else
+	  "#{base_title} | #{page_title}"
 	end
-
+  end
 
 end
