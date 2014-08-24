265ed1c9259903145fd911e8a37891a5cb4d0849
Lawrence Xia <xialairei@gmail.com>
2014-08-24 10:13:37 -0400

    cleaner title helper code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,8 @@ module ApplicationHelper
   #Takes title of client, and adds the title of the current page
   def full_title(page_title)
     base_title = Client.current.title
-    if page_title.empty?
-      base_title
-    else
-      base_title += " | #{page_title}"
-    end
+    base_title += " | #{page_title}" unless page_title.empty?
+    base_title
   end
 
 end
