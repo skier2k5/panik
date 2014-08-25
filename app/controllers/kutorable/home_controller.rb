978fd169634f3b0a2d324341e9efccd289ead0ba
Russell Stern <skier2k5@gmail.com>
2014-08-24 22:25:16 -0400

    Added changing of voting counts to ajax call

--- app/controllers/kutorable/home_controller.rb
+++ app/controllers/kutorable/home_controller.rb
@@ -1,5 +1,12 @@
 class Kutorable::HomeController < TrackedController
   def random_animal_picture
-    render :json => AnimalPicture.all.order("rand()").first
+    picture = AnimalPicture.all.order("rand()").first
+    random_picture = {
+      :id => picture.id,
+      :file_name => picture.file_name,
+      :voting_aggregate => picture.voting_aggregate,
+    }
+
+    render :json => random_picture
   end
 end
